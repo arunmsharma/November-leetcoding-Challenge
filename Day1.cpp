c7cf4ea7b1ebb7a213ad61e951501d40611fead4
arunmsharma <arunm2470@gmail.com>
2020-11-01 14:01:30 +0530

    Day 1

---
+++ Day1.cpp
@@ -0,0 +1,22 @@
+//day 1
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int answer = head->val;
+        while(head->next){
+            answer = (answer<<1) | (head->next->val);
+            head = head->next;
+        }
+        return answer;
+    }
+};
